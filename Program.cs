707ad134e2e050b9d73836715f28fb83b5e487d8
louis KALOMBO <valerykalombo@hotmail.fr>
2022-08-18 16:29:06 +0200

    ajout de requête

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using interaction_bdd_crud.Models;
 
+//Create method
 static ushort CreateArtist(disquaireContext ctx, string name, DateOnly birthday)
 {
     Artist artistToCreate = new Artist() { Name = name, Birthday = birthday };
@@ -7,14 +8,14 @@ static ushort CreateArtist(disquaireContext ctx, string name, DateOnly birthday)
     ctx.SaveChanges();
     return artistToCreate.Id;
 }
-
+//Update method
 static void UpdateArtist(disquaireContext ctx, string name, DateOnly birthday, ushort id){
     Artist artistToUpdate = ctx.Artists.Find(id);
     artistToUpdate.Name = name;
     artistToUpdate.Birthday = birthday;
     ctx.SaveChanges();
 }
-
+//Delete method
 static void DeleteArtist(disquaireContext ctx, ushort id) 
 {
      Artist artistToRemove = ctx.Artists.Find(id);
@@ -24,17 +25,25 @@ static void DeleteArtist(disquaireContext ctx, ushort id)
 }
 
 
-
-
-
-
-
-
-
-
-
 static void Main(string[] args)
 {
+    ushort idArtist = CreateArtist(ctx, "kobe", 1982/01/01);
+    UpdateArtist(ctx,"lebron", 1982/01/01);
+    DeleteArtist(ctx, idArtist);
+
+
+//requêtes sur des objets enumerable
+   using (disquaireContext ctx = new disquaireContext()) 
+    {
+        //afficher en console la list des artistes qui commencent par v
+         List<Artist> artists = ctx.Artists.Where(a=>a.Name.StartsWith("v")).ToList();
+
+        foreach (Artist item in artists)
+	    {
+            Console.WriteLine("le nom est: " + item);
+	    }
+    }
+
     
 
 }
